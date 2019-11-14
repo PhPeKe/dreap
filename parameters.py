2e7cdbf1d1735472ed1690be9e04209a1f1868b0
PhPeKe <p.kersten@live.de>
2019-11-14 14:11:33 +0100

    description of paramters.py

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
-alpha = 40
-beta = 30
-noise = 0.3
+alpha = 40      # Importance of content
+beta = 30       # Importance of style
+noise = 0.3     # Initial "disturbance" of content image
 height = 1026
 width = 770
-cImg = "img/railway_scaled.jpg"
-sImg = "img/realist_sunset_style_full.jpg"
+cImg = "img/railway_scaled.jpg"             # content image
+sImg = "img/realist_sunset_style_full.jpg"  # style image
